5adaa0e295d4e0876aba0e64bc04bab6c3e35237
Victor Loginov <tkoe88@zoho.com>
2023-08-15 17:30:06 +0200

    Challenge completed, refactored

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 # Controller for handling tasks view
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -18,18 +18,14 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     # No need for app/views/restaurants/destroy.html.erb
     redirect_to tasks_path, status: :see_other
@@ -40,4 +36,52 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
+
+# Here is the original controller before it was refactored
+# class TasksController < ApplicationController
+#   def index
+#     @tasks = Task.all
+#   end
+
+#   def show
+#     @task = Task.find(params[:id])
+#   end
+
+#   def new
+#     @task = Task.new
+#   end
+
+#   def create
+#     @task = Task.new(task_params)
+#     @task.save
+#     redirect_to task_path(@task)
+#   end
+
+#   def edit
+#     @task = Task.find(params[:id])
+#   end
+
+#   def update
+#     @task = Task.find(params[:id])
+#     @task.update(task_params)
+#     redirect_to task_path(@task)
+#   end
+
+#   def destroy
+#     @task = Task.find(params[:id])
+#     @task.destroy
+#     # No need for app/views/restaurants/destroy.html.erb
+#     redirect_to tasks_path, status: :see_other
+#   end
+
+#   private
+
+#   def task_params
+#     params.require(:task).permit(:title, :details, :completed)
+#   end
+# end
