0c3948620b020dcf6ccfc03812664a32ab9f9258
Victor Loginov <tkoe88@zoho.com>
2023-08-15 16:29:47 +0200

    almost done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
