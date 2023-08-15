b2a2dbee96a936c38313ca4c652ecfb64748ca49
Victor Loginov <tkoe88@zoho.com>
2023-08-15 14:59:00 +0200

    Create new task

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/tasks', to: 'tasks#index'
-  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id/edit', to: 'tasks#edit'
   patch '/tasks/:id', to: 'tasks#update'
